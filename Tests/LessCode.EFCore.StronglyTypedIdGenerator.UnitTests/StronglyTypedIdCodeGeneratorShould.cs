788cbed5d034f4d5c9fc731d9362690ef85a650c
杨中科 <yangzhongke8@gmail.com>
2025-01-02 23:38:28 +1300

    first test done

--- Tests/LessCode.EFCore.StronglyTypedIdGenerator.UnitTests/StronglyTypedIdCodeGeneratorShould.cs
+++ Tests/LessCode.EFCore.StronglyTypedIdGenerator.UnitTests/StronglyTypedIdCodeGeneratorShould.cs
@@ -3,6 +3,7 @@ using Microsoft.CodeAnalysis;
 using System.Reflection;
 using LessCode.EFCore.StronglyTypedId;
 using FluentAssertions;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace LessCode.EFCore.StronglyTypedIdGenerator.UnitTests
 {
@@ -38,6 +39,50 @@ namespace EntitiesProject2
             generatorResult.Generator.Should().Be(sut);
             generatorResult.GeneratedSources.Should().HaveCount(1);
             generatorResult.Exception.Should().BeNull();
+
+            var generatedIdClassSyntaxTree = generatorResult.GeneratedSources.Single().SyntaxTree;
+            var root = generatedIdClassSyntaxTree.GetRoot();
+            var namespaceDeclaration = root.DescendantNodes().OfType<NamespaceDeclarationSyntax>().SingleOrDefault();
+            namespaceDeclaration.Should().NotBeNull();
+            namespaceDeclaration.Name.ToString().Should().Be("EntitiesProject2");
+
+            var structDeclaration = namespaceDeclaration.DescendantNodes().OfType<StructDeclarationSyntax>().SingleOrDefault();
+            structDeclaration.Should().NotBeNull();
+            structDeclaration.Identifier.Text.Should().Be("CatId");
+            structDeclaration.Modifiers.Any(SyntaxKind.ReadOnlyKeyword).Should().BeTrue();
+
+            var constructorDeclarations = structDeclaration.DescendantNodes().OfType<ConstructorDeclarationSyntax>().ToArray();
+            constructorDeclarations.Should().HaveCount(2);
+
+            constructorDeclarations[0].Identifier.Text.Should().Be("CatId");
+            constructorDeclarations[1].Identifier.Text.Should().Be("CatId");
+
+            constructorDeclarations[0].Modifiers.Any(SyntaxKind.PublicKeyword).Should().BeTrue();
+            constructorDeclarations[1].Modifiers.Any(SyntaxKind.PublicKeyword).Should().BeTrue();
+
+            constructorDeclarations.Should().ContainSingle(c => c.ParameterList.Parameters.Count == 0);//parameterless constructor
+            constructorDeclarations.Should().ContainSingle(c => c.ParameterList.Parameters.Count == 1);//public CatId(long value) => Value = value;
+            constructorDeclarations.Should().ContainSingle(c => c.ParameterList.Parameters.Count == 1 && (c.ParameterList.Parameters.Single().Type as PredefinedTypeSyntax).Keyword.ValueText=="long");
+            
+            var fieldDeclarations = structDeclaration.DescendantNodes().OfType<FieldDeclarationSyntax>().ToArray();
+            fieldDeclarations.Should().HaveCountGreaterThanOrEqualTo(2);
+            fieldDeclarations[0].Declaration.Variables.Single().Identifier.Text.Should().Match("Converter*");
+            fieldDeclarations[1].Declaration.Variables.Single().Identifier.Text.Should().Match("Converter*");
+
+            var methodDeclarations = structDeclaration.DescendantNodes().OfType<MethodDeclarationSyntax>().ToArray();
+            methodDeclarations.Should().HaveCountGreaterThanOrEqualTo(3);
+            methodDeclarations.Any(m => m.Identifier.Text == "ToString").Should().BeTrue();
+            methodDeclarations.Any(m => m.Identifier.Text == "GetHashCode").Should().BeTrue();
+            methodDeclarations.Any(m => m.Identifier.Text == "Equals").Should().BeTrue();
+
+            var operatorDeclarations = structDeclaration.DescendantNodes().OfType<OperatorDeclarationSyntax>().ToArray();
+            operatorDeclarations.Should().HaveCountGreaterThanOrEqualTo(6);
+            operatorDeclarations.Any(o => o.OperatorToken.Text == "==").Should().BeTrue();
+            operatorDeclarations.Any(o => o.OperatorToken.Text == "!=").Should().BeTrue();
+            operatorDeclarations.Any(o => o.OperatorToken.Text == ">").Should().BeTrue();
+            operatorDeclarations.Any(o => o.OperatorToken.Text == "<").Should().BeTrue();
+            operatorDeclarations.Any(o => o.OperatorToken.Text == ">=").Should().BeTrue();
+            operatorDeclarations.Any(o => o.OperatorToken.Text == "<=").Should().BeTrue();
         }
 
         private static Compilation CreateCompilation(string source)
