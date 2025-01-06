6b7ba18d3843ca10db4b543b2bc30383dd8a0bdf
杨中科 <yangzhongke8@gmail.com>
2025-01-06 20:12:18 +1300

    clean code

--- LessCode.EFCore.StronglyTypedIdGenerator/IdValueConverterTemplate.CodeBehind.cs
+++ LessCode.EFCore.StronglyTypedIdGenerator/IdValueConverterTemplate.CodeBehind.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace LessCode.EFCore.StronglyTypedIdGenerator
+﻿namespace LessCode.EFCore.StronglyTypedIdGenerator
 {
     public partial class IdValueConverterTemplate
     {
